0e2e0f02b1f8feff746e587f0945f19b8cbc2919
keke125 <admin@keke125.com>
2023-10-19 16:50:10 +0800

    feat: Integrate MariaDB using Spring Data JPA for storing Users' Information.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into user (version, id, username, name, hashed_password, avatar_image, avatar_image_name, email,
+                              enabled, is_account_non_expired, is_account_non_locked, is_credentials_non_expired)
+values (1, '1', 'admin', 'admin',
+        '{argon2}$argon2id$v=19$m=16384,t=2,p=1$S9swkCEbXj5O8cwbZdyrtQ$4RkxTGl3xkEfm51P/gc3FORCysPStozAsm6smOnQR18',
+        null, null, 'admin@example.com', true, true, true, true);
+insert into user_roles (user_id, roles)
+values ('1', 'USER');
+insert into user_roles (user_id, roles)
+values ('1', 'ADMIN');
