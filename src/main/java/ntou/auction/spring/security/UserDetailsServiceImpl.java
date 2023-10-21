1db5070aeac4e083b0f4fdf9ef7d0d9878c456b7
keke125 <admin@keke125.com>
2023-10-21 17:54:45 +0800

    style: Clean up and remove unused code.

--- src/main/java/ntou/auction/spring/security/UserDetailsServiceImpl.java
+++ src/main/java/ntou/auction/spring/security/UserDetailsServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
