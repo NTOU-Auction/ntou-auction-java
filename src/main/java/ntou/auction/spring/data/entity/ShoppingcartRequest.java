9d4ad52225bc11c258ea2f2afcb3c7120defb855
Ray <wang.ray0321@gmail.com>
2023-11-23 21:54:57 +0800

    feat: Introduce a REST Controller for Shoppingcart with error handling.

---
+++ src/main/java/ntou/auction/spring/data/entity/ShoppingcartRequest.java
@@ -0,0 +1,14 @@
+package ntou.auction.spring.data.entity;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShoppingcartRequest {
+    @NotNull
+    private Long productId;
+}
